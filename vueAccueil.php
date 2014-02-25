0513b468ec4086e60b6617a26c8b6b23ad9316a8
FaCoInf <corinne.fagno@gmail.com>
2014-02-25 17:59:04 +0100

    Ajout modele vueAccueil

--- vueAccueil.php
+++ vueAccueil.php
@@ -1,8 +1,31 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Mon Blog</title>
+    </head>
+    <body>
+        <div id="global">
+            <header>
+                <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
+                <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
+            </header>
+            <div id="contenu">
+                <?php foreach ($billets as $billet):?>
+                    <article>
+                        <header>
+                            <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+                            <time><?= $billet['date'] ?></time>
+                        </header>
+                        <p><?= $billet['contenu'] ?></p>
+                    </article>
+                    <hr />
+                <?php endforeach; ?>
+            </div> <!-- #contenu -->
+            <footer id="piedBlog">
+                Blog réalisé avec PHP, HTML5 et CSS.
+            </footer>
+        </div> <!-- #global -->
+    </body>
+</html>
